72c6b4b3895f88a61ae9142755c8af2911870687
tuanndph16237 <tuanndph16237@example.com>
2022-09-12 22:26:55 +0700

    demo angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +6,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angulardemo';
+  student={
+    FullName:'Nguyen Duc Tuan',
+  age: 20,
+  Gioitinh:'Nam',
+DateofBirth: '05/10/2002',
+images: '../assets/images/Visual_Studio_Code_1.35_icon.svg.png',
+diemTB:6.0
+  }
+
 }
